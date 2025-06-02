bd9a890b2bf067cf038b7304e80ec4bebda7f453
John Irungu <jjohnirungu@gmail.com>
2025-06-02 11:58:18 +0300

    remove the unused data sections

--- src/data/navItem.js
+++
@@ -1,7 +0,0 @@
-export const navItems = [
-    { name: 'Home', href: '#home' },
-    { name: 'About', href: '#about' },
-    { name: 'Skills', href: '#skills' },
-    { name: 'Experience', href: '#experience' },
-    { name: 'Contact', href: '#contact' }
-  ]
--- src/data/techStack.js
+++
@@ -1,6 +0,0 @@
-export const techStack = [
-    { name: 'AWS Certified', icon: 'AWS', bgClass: 'bg-blue-600' },
-    { name: 'Kubernetes', icon: 'K8s', bgClass: 'bg-blue-600' },
-    { name: 'Laravel', icon: 'PHP', bgClass: 'bg-blue-600' },
-    { name: 'Node.js', icon: 'JS', bgClass: 'bg-blue-600' }
-  ]
